f57ff675937d4525611a2140252ac5be40a70f5c
omar-sarfraz <omar786089@gmail.com>
2023-01-05 21:59:05 +0500

    Security Update

--- firebase.js
+++ firebase.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3wglBzTq8QKgvL9QUs2swnmYycglga00",
-  authDomain: "test-61e7b.firebaseapp.com",
-  databaseURL:
-    "https://test-61e7b-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "test-61e7b",
-  storageBucket: "test-61e7b.appspot.com",
-  messagingSenderId: "372653385617",
-  appId: "1:372653385617:web:05a48e3665d7b3335edc30",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DB_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.ST_BUCKET,
+  messagingSenderId: process.env.MSG_ID,
+  appId: process.env.APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
